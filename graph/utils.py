9208ca7f6bb368a9fbd9ec665e2f925212c82ba3
05kashyap <aryankashyapnaveen@gmail.com>
2023-11-11 11:36:27 +0530

    updated community detection algorithm

--- graph/utils.py
+++ graph/utils.py
@@ -5,6 +5,7 @@ import networkx as nx
 from users.models import Profile
 from collections import OrderedDict
 from django.contrib.auth.models import User
+import networkx.algorithms.community as nxcom
 
 
 def get_top_n(dict_, n):
@@ -19,6 +20,32 @@ def get_graph():
     graph = graph.decode('utf-8')
     buffer.close()
     return graph
+
+def set_node_community(G, communities):
+        '''Add community to node attributes'''
+        for c, v_c in enumerate(communities):
+            for v in v_c:
+                # Add 1 to save 0 for external edges
+                G.nodes[v]['community'] = c + 1
+def set_edge_community(G):
+    '''Find internal edges and add their community to their attributes'''
+    for v, w, in G.edges:
+        if G.nodes[v]['community'] == G.nodes[w]['community']:
+            # Internal edge, mark with community
+            G.edges[v, w]['community'] = G.nodes[v]['community']
+        else:
+            # External edge, mark as 0
+            G.edges[v, w]['community'] = 0
+def get_color(i, r_off=1, g_off=1, b_off=1):
+    '''Assign a color to a vertex.'''
+    r0, g0, b0 = 0, 0, 0
+    n = 16
+    low, high = 0.1, 0.9
+    span = high - low
+    r = low + span * (((i + r_off) * 3) % n) / (n - 1)
+    g = low + span * (((i + g_off) * 5) % n) / (n - 1)
+    b = low + span * (((i + b_off) * 7) % n) / (n - 1)
+    return (r, g, b)
 
 def get_plot():
 
@@ -53,26 +80,31 @@ def get_plot():
         for follower in followers:
             Social.add_edge(follower, user)
 
-    communities = nx.community.girvan_newman(Social)
-    k = len(Social.nodes())//4 +1
-    for i in range(k):
-        community = next(communities)
+    
 
-    # Assign a color to each community for node coloring
-    node_colors = {}
-    for i, com in enumerate(community):
-        for node in com:
-            node_colors[node] = i
-
-    # Convert the node_colors dictionary to a list for coloring nodes
-    colors = [node_colors[node] for node in Social.nodes()]
     d = dict(Social.degree)# used for size increase based on degree
 
-    pos = nx.spring_layout(Social, k=0.2, iterations=2)
+    communities = sorted(nxcom.greedy_modularity_communities(Social), key=len, reverse=True)
+    
+    print(f"The graph has {len(communities)} communities.")
+    set_node_community(Social, communities)
+    set_edge_community(Social)
+    node_color = [get_color(Social.nodes[v]['community']) for v in Social.nodes]
+    # Set community color for edges between members of the same community (internal) and intra-community edges (external)
+    external = [(v, w) for v, w in Social.edges if Social.edges[v, w]['community'] == 0]
+    internal = [(v, w) for v, w in Social.edges if Social.edges[v, w]['community'] > 0]
+    internal_color = ['black' for e in internal]
+    pos = nx.spring_layout(Social, k=0.4, iterations=2)
     #pos = nx.kamada_kawai_layout(Social)
     plt.figure(figsize=(10,6))
 
-    nx.draw(Social, pos, node_color=colors, with_labels=True, node_size=[v * 100 for v in d.values()], style='dashed',cmap=plt.cm.Pastel1)
+    nx.draw_networkx(Social, pos,edge_color="silver",edgelist = external, with_labels=True, node_size=[v * 100 for v in d.values()])
+    nx.draw_networkx(
+        Social,
+        pos=pos,
+        node_color=node_color,
+        edgelist=internal,
+        edge_color=internal_color, node_size=[v * 100 for v in d.values()])
     #nx.draw(Social, pos, with_labels=True)
     #edge_labels = dict([((u,v,), d['weight']) for u,v,d in Social.edges(data=True)])
 
