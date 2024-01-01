185b4a959e79b658fc023015a3abbbfa65bb56d0
Aryan Kashyap Naveen <120780494+05kashyap@users.noreply.github.com>
2024-01-01 09:38:58 +0530

    Update models.py

--- feed/models.py
+++ feed/models.py
@@ -53,7 +53,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return self.body
 
 
 class LikeManager(models.Manager):
