1ae36066177fa34f9ca4f0d4870ee5d09b1b5b3a
Aryan Kashyap Naveen <120780494+05kashyap@users.noreply.github.com>
2024-01-17 15:39:36 +0530

    Update settings.py

--- graphlink/settings.py
+++ graphlink/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-ri%dvnbo_$2-xyif$ncqrvyg^2ky9wfa*073*8&q(r73ks8i'
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
