cd379f8725cb4d1f966bbf0ff905ba18ad2e228a
05kashyap <aryankashyapnaveen@gmail.com>
2024-02-18 19:42:12 +0530

    Updated home UI

--- graph/views.py
+++ graph/views.py
@@ -3,7 +3,7 @@ from django.core.files import File
 # Create your views here.
 import matplotlib.pyplot as plt
 import networkx as nx
-from .utils import get_plot, generate_user_follow_suggestions
+from .alterutil import get_plot as plot, generate_suggestions as suggestions
 from django.conf import settings
 from users.models import Profile
 
@@ -11,7 +11,7 @@ from users.models import Profile
 
 
 def plot_network(request):
-
-    chart = get_plot() 
-    recs = generate_user_follow_suggestions(request.user.profile)
-    return render(request, 'graph/network.html', {'chart':chart, 'suggestions': recs, 'user' : request.user})
+    """ this will call other neccessary function to build the graph and present it """
+    chart, social = plot()
+    recs, followbacks = suggestions(request.user.profile)
+    return render(request, 'graph/network.html', {'chart':chart, 'suggestions': recs, 'followback':followbacks, 'social':social})
